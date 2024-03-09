4324270f102001b86f6c8289f8768c51466b8308
lyancsie <info@fordit.biz>
2024-03-09 15:11:06 +0100

    start refactoring

--- NumberConverter/src/NumberConverter.java
+++ NumberConverter/src/NumberConverter.java
@@ -16,11 +16,23 @@ public class NumberConverter {
     private static final String[] ONE_TO_NINETEEN = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
     private static final String[] TWENTY_TO_NINETY = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
     private static final String HUNDRED = "hundred";
-    private static final String THOUSAND = "thousand";
-    private static final String MILLION = "million";
-    private static final String BILLION = "billion";
-    private static final String TRILLION = "trillion";
+    private static final String[] THOUSANDS = {"trillion", "billion", "million", "thousand"};
+    private static final long[] NUMBERS_TO_DIVIDE_WITH = {100_000_000_000_000L, 100_000_000_000L, 1_000_000, 1000};
     private static final String HYPHEN = "-";
+
+    static String appendThousands(long number, String numberInEnglish, int iteration) {
+        int hundreds = getHundreds(number, iteration);
+        numberInEnglish += " " + ONE_TO_NINETEEN[hundreds] + " " + HUNDRED;
+        number -= hundreds * NUMBERS_TO_DIVIDE_WITH[iteration];
+        if (number < NUMBERS_TO_DIVIDE_WITH[iteration] / 100) {
+            numberInEnglish += " " + THOUSANDS[iteration];
+        }
+        return numberInEnglish;
+    }
+
+    private static int getHundreds(long number, int iteration) {
+        return (int) (number / NUMBERS_TO_DIVIDE_WITH[iteration]);
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -29,79 +41,61 @@ public class NumberConverter {
         String numberInEnglish = "";
         // nine hundred ninety-nine billion nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine
 
-        if (number >= 100_000_000_000_000L){
-            int hundredtrillions = (int) (number / 100_000_000_000_000L);
-            numberInEnglish += " " + ONE_TO_NINETEEN[hundredtrillions] + " " + HUNDRED;
-            number -= hundredtrillions * 100_000_000_000_000L;
-            if (number < 1_000_000_000_000L){
-                numberInEnglish += " " + TRILLION;
-            }
-        }
-
-        if (number >=1_000_000_000_000L){
+        numberInEnglish = appendThousands(number, numberInEnglish, 0);
+        System.out.printf("%d - %d = %d",
+            number,
+            getHundreds(number, 0) * NUMBERS_TO_DIVIDE_WITH[0],
+            (number - getHundreds(number, 0) * NUMBERS_TO_DIVIDE_WITH[0]));
+        number -= getHundreds(number, 0) * NUMBERS_TO_DIVIDE_WITH[0];
+        //        number -= hundreds * NUMBERS_TO_DIVIDE_WITH[iteration - 1];
+        if (number >= 1_000_000_000_000L) {
             int trillions = (int) (number / 1_000_000_000_000L);
             if (trillions >= 20) {
-                numberInEnglish += " " + TWENTY_TO_NINETY[trillions / 10] + HYPHEN + ONE_TO_NINETEEN[trillions % 10] + " " + TRILLION;
+                numberInEnglish += " " + TWENTY_TO_NINETY[trillions / 10] + HYPHEN + ONE_TO_NINETEEN[trillions % 10] + " " + "trillion";
             } else {
-                numberInEnglish += " " + ONE_TO_NINETEEN[trillions] + " " + TRILLION;
+                numberInEnglish += " " + ONE_TO_NINETEEN[trillions] + " " + "trillion";
             }
             number -= trillions * 1_000_000_000_000L;
         }
 
-        if (number >= 100_000_000_000L) {
-            int hundredbillions = (int) (number / 100_000_000_000L);
-            numberInEnglish += " " + ONE_TO_NINETEEN[hundredbillions] + " " + HUNDRED;
-            number -= hundredbillions * 100_000_000_000L;
-            if (number < 1000_000_000) {
-                numberInEnglish += " " + BILLION;
-            }
-        }
+        numberInEnglish = appendThousands(number, numberInEnglish, 1);
+        number -= getHundreds(number, 1) * NUMBERS_TO_DIVIDE_WITH[1];
+
 
         if (number >= 1_000_000_000) {
             int billions = (int) (number / 1_000_000_000);
             if (billions >= 20) {
-                numberInEnglish += " " + TWENTY_TO_NINETY[billions / 10] + HYPHEN + ONE_TO_NINETEEN[billions % 10] + " " + BILLION;
+                numberInEnglish += " " + TWENTY_TO_NINETY[billions / 10] + HYPHEN + ONE_TO_NINETEEN[billions % 10] + " " + "billion";
             } else {
-                numberInEnglish += " " + ONE_TO_NINETEEN[billions] + " " + BILLION;
+                numberInEnglish += " " + ONE_TO_NINETEEN[billions] + " " + "billion";
             }
             number -= billions * 1_000_000_000L;
         }
 
-        if (number >= 100_000_000) {
-            int hundredmillions = (int) (number / 100_000_000);
-            numberInEnglish += " " + ONE_TO_NINETEEN[hundredmillions] + " " + HUNDRED;
-            number -= hundredmillions * 100_000_000;
-            if (number < 1_000_000) {
-                numberInEnglish += " " + MILLION;
-            }
-        }
+        numberInEnglish = appendThousands(number, numberInEnglish, 2);
+        number -= getHundreds(number, 2) * NUMBERS_TO_DIVIDE_WITH[2];
+
 
         if (number >= 1_000_000) {
             int millions = (int) (number / 1_000_000);
 
             if (millions >= 20) {
-                numberInEnglish += " " + TWENTY_TO_NINETY[millions / 10] + HYPHEN + ONE_TO_NINETEEN[millions % 10] + " " + MILLION;
+                numberInEnglish += " " + TWENTY_TO_NINETY[millions / 10] + HYPHEN + ONE_TO_NINETEEN[millions % 10] + " " + "million";
             } else {
-                numberInEnglish += " " + ONE_TO_NINETEEN[millions] + " " + MILLION;
+                numberInEnglish += " " + ONE_TO_NINETEEN[millions] + " " + "million";
             }
             number -= millions * 1_000_000L;
         }
 
-        if (number >= 100_000) {
-            int hundredthousands = (int) (number / 100_000);
-            numberInEnglish += " " + ONE_TO_NINETEEN[hundredthousands] + " " + HUNDRED;
-            number -= hundredthousands * 100_000;
-            if (number < 1000) {
-                numberInEnglish += " " + THOUSAND;
-            }
-        }
+        numberInEnglish = appendThousands(number, numberInEnglish, 3);
+        number -= getHundreds(number, 3) * NUMBERS_TO_DIVIDE_WITH[3];
 
         if (number >= 1000) {
             int thousands = (int) (number / 1000);
             if (thousands >= 20) {
-                numberInEnglish += " " + TWENTY_TO_NINETY[thousands / 10] + HYPHEN + ONE_TO_NINETEEN[thousands % 10] + " " + THOUSAND;
+                numberInEnglish += " " + TWENTY_TO_NINETY[thousands / 10] + HYPHEN + ONE_TO_NINETEEN[thousands % 10] + " " + "thousand";
             } else {
-                numberInEnglish += " " + ONE_TO_NINETEEN[thousands] + " " + THOUSAND;
+                numberInEnglish += " " + ONE_TO_NINETEEN[thousands] + " " + "thousand";
             }
             number -= thousands * 1000L;
         }
